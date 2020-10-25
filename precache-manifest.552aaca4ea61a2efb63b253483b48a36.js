cd1e071bf3e6bcc65cacc929184680cec634716e
n5kumarx <n5kumarx@yahoo.co.in>
2020-10-26 00:11:40 +0530

    Updates

---
+++ precache-manifest.552aaca4ea61a2efb63b253483b48a36.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d12d16af8f0d78d5623867330e1f91",
+    "url": "/omnisenseconsulting/index.html"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/omnisenseconsulting/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "85c079e5752137aa5711",
+    "url": "/omnisenseconsulting/static/css/main.8b58b3cd.chunk.css"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/omnisenseconsulting/static/js/2.670fdb07.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/omnisenseconsulting/static/js/2.670fdb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c079e5752137aa5711",
+    "url": "/omnisenseconsulting/static/js/main.964a1084.chunk.js"
+  },
+  {
+    "revision": "b3eedd5d30f8dbca9a7f",
+    "url": "/omnisenseconsulting/static/js/runtime-main.ef42513d.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/omnisenseconsulting/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/omnisenseconsulting/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/omnisenseconsulting/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/omnisenseconsulting/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/omnisenseconsulting/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/omnisenseconsulting/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/omnisenseconsulting/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/omnisenseconsulting/static/media/software.43049cae.jpeg"
+  }
+]);
